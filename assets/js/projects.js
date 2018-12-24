c48e7de8f321c026d5effd89072dfca32226efe6
briancnguyen <briancnguyen@berkeley.edu>
2018-12-24 04:05:15 -0800

    Finished project pic and templateing for html

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $('.preloader').delay(200).fadeOut('fast');
     $('.preloader-background').delay(200).fadeOut('slow');
     $('.sidenav').sidenav();
+    $('.slider').slider({'height': 500});
 });
