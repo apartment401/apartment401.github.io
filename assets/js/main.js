587a7eefffcaafad0507c8387e58025c58d32be0
Brian Nguyen <briancnguyen@berkeley.edu>
2019-06-01 01:57:23 -0700

    cleaned up home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,30 +3,21 @@ $(document).ready(function(){
     $('.preloader-background').delay(1500).fadeOut('slow');
     $('.sidenav').sidenav();
     $('.parallax').parallax();
-    $('.tabs').tabs();
     $('.slider').slider({'height': 500});
+    $('ul.tabs').tabs();
     // Add smooth scrolling to all links
     $('a')
     .not('[type="panel"]')
     .on('click', function(event) {
-
-        // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
+            console.log("Move!")
             event.preventDefault();
-    
-            // Store hash
             var hash = this.hash;
-    
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
             scrollTop: $(hash).offset().top
             }, 800, function(){
-        
-            // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
             });
-        } // End if
+        }
     });
 });
